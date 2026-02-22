17b965c99ae78b3059478fbcf2e137b028a20685
Luriannys <ljunco@correo.unimet.edu.ve>
2026-02-21 21:12:24 -0400

    Paso 1

---
+++ main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional
+
+app = FastAPI()
+
+class Libro(BaseModel):
+    titulo: str
+    autor: str
+    paginas: int
+    editorial: Optional[str]
+
+#Decorador
+@app.get("/")
+def index():
+    return {"message" : "Hello, World!"}
+
+@app.get("/libros/{id}")
+def mostrar_libro(id: int):
+    return {"data": id}
+
+@app.post("/libros/")
+def crear_libro(libro: Libro):
+    return {"message": f"Libro {libro.titulo} ha sido creado"}
